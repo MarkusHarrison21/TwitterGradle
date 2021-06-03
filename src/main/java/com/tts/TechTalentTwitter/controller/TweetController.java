8568a3f21347afa0c717b2aac947fbbe9599811a
Markus H <meharrison2002@gmail.com>
2021-06-03 09:30:52 -0400

    added timeline for those a user follows, shortened links

--- src/main/java/com/tts/TechTalentTwitter/controller/TweetController.java
+++ src/main/java/com/tts/TechTalentTwitter/controller/TweetController.java
@@ -1,9 +1,11 @@
 package com.tts.TechTalentTwitter.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.validation.Valid;
 
+import com.tts.TechTalentTwitter.model.TweetDisplay;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +18,7 @@ import com.tts.TechTalentTwitter.model.Tweet;
 import com.tts.TechTalentTwitter.model.User;
 import com.tts.TechTalentTwitter.service.TweetService;
 import com.tts.TechTalentTwitter.service.UserService;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TweetController {
@@ -31,11 +34,31 @@ public class TweetController {
     }
 
 
-    @GetMapping(value= {"/tweets", "/"})
-    public String getFeed(Model model){
-        List<Tweet> tweets = tweetService.findAll();
+    @GetMapping(value = { "/tweets", "/" })
+    public String getFeed(@RequestParam(value = "filter", required = false) String filter, Model model) {
+        User loggedInUser = userService.getLoggedInUser();
+        List<TweetDisplay> tweets = new ArrayList<>();
+        if (filter == null) {
+            filter = "all";
+        }
+        if (filter.equalsIgnoreCase("following")) {
+            List<User> following = loggedInUser.getFollowing();
+            tweets = tweetService.findAllByUsers(following);
+            model.addAttribute("filter", "following");
+        } else {
+            tweets = tweetService.findAll();
+            model.addAttribute("filter", "all");
+        }
         model.addAttribute("tweetList", tweets);
         return "feed";
+    }
+
+    @GetMapping(value = "/tweets/{tag}")
+    public String getTweetsByTag(@PathVariable(value="tag") String tag, Model model) {
+        List<TweetDisplay> tweets = tweetService.findAllWithTag(tag);
+        model.addAttribute("tweetList", tweets);
+        model.addAttribute("tag", tag);
+        return "taggedTweets";
     }
 
     @GetMapping(value = "/tweets/new")
@@ -43,6 +66,7 @@ public class TweetController {
         model.addAttribute("tweet", new Tweet());
         return "newTweet";
     }
+
 
     @PostMapping(value = "/tweets")
     public String submitTweetForm(@Valid Tweet tweet, BindingResult bindingResult, Model model) {
@@ -56,12 +80,6 @@ public class TweetController {
         return "newTweet";
     }
 
-    @GetMapping(value = "/tweets/{tag}")
-    public String getTweetsByTag(@PathVariable(value="tag") String tag, Model model) {
-        List<Tweet> tweets = tweetService.findAllWithTag(tag);
-        model.addAttribute("tweetList", tweets);
-        model.addAttribute("tag", tag);
-        return "taggedTweets";
-    }
+
 
 }
